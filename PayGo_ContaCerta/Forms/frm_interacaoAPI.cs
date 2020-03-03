2a4c1b651b00afc91afe50984f41b7039366208d
Iago Rocha Leite <iagop9@hotmail.com>
2020-03-03 14:16:58 -0300

    Método de Micro_Deposito criado.

--- PayGo_ContaCerta/Forms/frm_interacaoAPI.cs
+++ PayGo_ContaCerta/Forms/frm_interacaoAPI.cs
@@ -17,28 +17,26 @@ namespace PayGo_ContaCerta.Forms
             InitializeComponent();
         }
 
-        private System.Windows.Forms.Timer timer1;
-        private int counter = 1800000; //1800 segundos
+        #region Expiração key
+        //private System.Windows.Forms.Timer timer1;
+        //private int counter = 1800000; //1800 segundos
+        //int counter = 1800000;
+        //Timer timer = new Timer();
+        //timer.Tick += new EventHandler(GerarNovaKey);
+        //timer.Interval = 1000; //1 segundo
+        //timer.Start();
+        #endregion
 
         private async void btn_Post_Click(object sender, EventArgs e)
         {
-            var response = await RestHelper.Post_Authentication("client_credentials", "2b49c7d3-6b3f-44d4-9630-885d8ffde831", "5738a69a-8da5-44a9-b40d-95eb747a703a0604fc78-4c0f-4301-a8ee-fb449c85e7a7");
+            var response = string.Empty;
+            response = await RestHelper.Post_Authentication("client_credentials", "2b49c7d3-6b3f-44d4-9630-885d8ffde831", "5738a69a-8da5-44a9-b40d-95eb747a703a0604fc78-4c0f-4301-a8ee-fb449c85e7a7");
+            //txt_PostResponse.Text = RestHelper.BeautifyJson(response);
+
+            response = await RestHelper.Post_MicroDeposito("Iago Rocha Leite", "45161928883", "341", "0068", "", "07734", "6", "CONTA_CORRENTE", "");
             txt_PostResponse.Text = RestHelper.BeautifyJson(response);
-
-
-
-
-            int counter = 1800000;
-            Timer timer = new Timer();
-            timer.Tick += new EventHandler(GerarNovaKey);
-            timer.Interval = 1000; //1 segundo
-            timer.Start();
-
         }
 
-        private void GerarNovaKey(object sender, EventArgs e)
-        {
-
-        }
+        
     }
 }
--- PayGo_ContaCerta/RestHelper.cs
+++ PayGo_ContaCerta/RestHelper.cs
@@ -4,14 +4,25 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace PayGo_ContaCerta
 {
     public static class RestHelper
-    { 
+    {
         private static readonly string baseURL = "https://login-api-sandbox.transfeera.com/";
+        private static readonly string baseURLContaCerta = "https://contacerta-api-sandbox.transfeera.com/";
+        private static string keyAutorization;
+
+        private static void SetKeyAutorization(string key)
+        {
+            string[] splt = key.Split('"');
+            string keyTratada = splt[3].ToString().Trim();
+
+            keyAutorization = keyTratada;
+        }
 
         public static async Task<string> Post_Authentication(string gt, string ci, string cs)
         {
@@ -27,6 +38,73 @@ namespace PayGo_ContaCerta
             using (HttpClient client = new HttpClient())
             {
                 using (HttpResponseMessage res = await client.PostAsync(baseURL + "authorization", input))
+                {
+                    using (HttpContent content = res.Content)
+                    {
+                        string data = await content.ReadAsStringAsync();
+                        if (data != null)
+                        {
+                            SetKeyAutorization(data);
+                            return data;
+                        }
+                    }
+                }
+            }
+
+            return string.Empty;
+        }
+
+        public static async Task<string> Post_MicroDeposito(string name, string cpfCnpj, string bankCode, string agc, string agcDigit,
+            string acct, string acctDigit, string acctType, string itgId)
+        {
+            var inputData = new Dictionary<string, string>
+            {
+                {"name", name},
+                {"cpf_cnpj", cpfCnpj},
+                {"bank_code", bankCode},
+                {"agency", agc},
+                {"agency_digit", agcDigit},
+                {"account", acct},
+                {"account_digit", acctDigit},
+                {"account_type", acctType},
+                {"integration_id", itgId}
+            };
+
+            var input = new FormUrlEncodedContent(inputData);
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(baseURLContaCerta);
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/x-www-form-urlencoded"));
+                //client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjhmOGY3OTNmLTBkZjEtNGEwNy05OGQ5LTY0ZThkNWQ0ZjgxMyIsIm5hbWUiOiJDb250YUNlcnRhIENvbnRhQ2VydGEgUGF5R28iLCJlbWFpbCI6ImFydGh1ci5uYXNjaW1lbnRvQHBheWdvLmNvbS5iciIsInN0YXR1cyI6IkhBQklMSVRBRE8iLCJoYXNfdHdvX2ZhY3Rvcl9lbmFibGVkIjpmYWxzZSwicHJvZmlsZSI6IkFETUlOIiwiY29tcGFueSI6eyJpZCI6ImU3ZTgyZjNkLWQ5ZTgtNDgxNy1iNWExLTRmZWJkMTVhNjQ3YSIsIm5hbWUiOiJDb250YUNlcnRhIENvbnRhQ2VydGEgUGF5R28iLCJwaG9uZSI6IiAgICAgICAgICAgIiwiY3BmQ25waiI6IjY2MjEyNTI0MDAwMTU0IiwibG9nbyI6IiIsImFjY2VwdGVkQ29udHJhY3QiOnRydWUsInN0YXR1cyI6IkFDVElWRSIsImNyZWF0ZWRBdCI6IjIwMTktMTEtMTFUMTQ6NTM6NDMuMTc5NzYxLTAzOjAwIn0sImlhdCI6MTU4Mjc0NzgyMywiZXhwIjoxNTgyNzQ5NjIzfQ.gfAYp7Sz2UNQOrXVZrqeGj3RkS6d9zhF3hrTiRYRd7U");
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(keyAutorization);
+
+                using (HttpResponseMessage res = await client.PostAsync(baseURLContaCerta + "validate?type=MICRO_DEPOSITO", input))
+                {
+                    using (HttpContent content = res.Content)
+                    {
+                        string data = await content.ReadAsStringAsync();
+                        if (data != null)
+                        {
+                            return data;
+                        }
+                    }
+                }
+            }
+            return string.Empty;
+        }
+
+        public static async Task<string> Post_MicroDeposito(string url)
+        {
+            var inputData = new Dictionary<string, string>
+            {
+                {"url", url}
+            };
+
+            var input = new FormUrlEncodedContent(inputData);
+
+            using (HttpClient client = new HttpClient())
+            {
+                using (HttpResponseMessage res = await client.PostAsync(baseURL + "webhook", input))
                 {
                     using (HttpContent content = res.Content)
                     {
